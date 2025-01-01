a7363212bce6c6dc02b72238a867ad0933d59466
hkc619 <kendrick10112001@gmail.com>
2025-01-01 17:58:09 +0800

    Add task func, need to adjust. 0101, 2025

--- services/addTask.js
+++ services/addTask.js
@@ -1,7 +1,10 @@
+import * as fs from "fs";
+import data from "../data.json" assert { type: "json" };
+
 import * as path from "path";
 import { basename, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-import data from "./data.json" assert { type: "json" };
+import { json } from "node:stream/consumers";
 
 /*
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +15,20 @@ console.log(path.join(__dirname, "/data.json"));
 const dataPath = path.join(__dirname, "/data.json");
 const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
 const countries = loadJSON('./data/countries.json');
-
 */
-console.log(data);
+var newTask = "test0101";
+var newTaskId = data.task.length + 1;
+var newTaskstatus = "todo";
+
+var newTaskJson = JSON.stringify({
+  id: newTaskId,
+  task: newTask,
+  staus: newTaskstatus,
+});
+console.log(newTaskJson);
+
+fs.writeFile("../data.json", newTaskJson, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
