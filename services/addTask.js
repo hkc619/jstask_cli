31e839ea570fe7ef2117c7bf81b3e3ab6352c4d1
hkc619 <kendrick10112001@gmail.com>
2025-01-01 17:09:37 +0800

    Import local json in easy way. 0101,2025

--- services/addTask.js
+++ services/addTask.js
@@ -1,16 +1,17 @@
 import * as path from "path";
 import { basename, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
+import data from "./data.json" assert { type: "json" };
 
+/*
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const filename = basename(__filename);
-
 console.log(path.dirname(__filename));
 console.log(path.join(__dirname, "/data.json"));
-
 const dataPath = path.join(__dirname, "/data.json");
+const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+const countries = loadJSON('./data/countries.json');
 
-fetch("./data.json")
-  .then((res) => res.json())
-  .then((json) => console.log(json));
+*/
+console.log(data);
