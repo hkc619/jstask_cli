eb3d9600923777346e4a05882739e975ad979202
hkc619 <kendrick10112001@gmail.com>
2025-01-02 19:53:28 +0800

    Cannot write in json. 0102, 2025

--- index.js
+++ index.js
@@ -2,19 +2,23 @@
 
 import { Command } from "commander";
 import data from "./data.json" assert { type: "json" };
+import { addTask } from "./services/addTask.js";
 
 const program = new Command();
 
-program
-  .version("0.0.1")
-  .usage("[options]")
-  .option("-n, --name", "Your name.")
+program.version("0.0.1").usage("[options]").option("-n, --name", "Your name.");
+/*
   .option("-a, --add", "Add a new task.")
   .option("-u, --update", "Update a task.")
   .option("-d, --delete", "Delete a task.")
   .option("-l, --list", "A list of seleted tasks.");
-
+  */
+program
+  .command("add")
+  .argument("<string>", "Task you want to add.")
+  .action((str, options) => {
+    console.log(str);
+    console.log(options);
+    console.log(`Task added successfully (ID: ${addTask(str)})`);
+  });
 program.parse();
-const options = program.opts();
-console.log(program);
-console.log("Hello, %s.", program.args[0] || "world");
