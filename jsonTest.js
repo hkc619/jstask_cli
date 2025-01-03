e89f8b532072f90e95325de01e65ba8dce196b8b
hkc619 <kendrick10112001@gmail.com>
2025-01-03 20:09:32 +0800

    Test write and read json. 0103, 2025

--- jsonTest.js
+++ jsonTest.js
@@ -1,3 +1,18 @@
 import data from "./data.json" assert { type: "json" };
-
+import * as fs from "fs";
 console.log(data.task.length);
+
+fs.readFile("../data.json", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  var json = JSON.parse(data);
+  json["task"].push({ id: 3, task: "test0101", staus: "todo" });
+  data = JSON.stringify(json);
+  console.log(data);
+  fs.writeFile("../data.json", data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
